afcd03df31ea38856a31dd02499b13fe3f64f6e2
Amit Kumar <amit.sengar@tarento.com>
2021-05-06 17:22:27 +0530

    init commit

---
+++ project/ws/app/src/lib/routes/home/home.rounting.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+// import { InitResolver } from './resol./routes/profile-v2/discuss-all.component'
+import { HomeResolve } from './resolvers/home-resolve'
+import { AboutComponent } from './routes/about/about.component'
+import { HomeComponent } from './routes/home/home.component'
+import { UsersViewComponent } from './routes/users-view/users-view.component'
+import { RolesAccessComponent } from './routes/roles-access/roles-access.component'
+import { DirectoryViewComponent } from './routes/directory/directroy.component'
+// import { CreateMdoComponent } from './routes/create-mdo/create-mdo.component'
+// import { CreateUserComponent } from './routes/create-user/create-user.component'
+import { DepartmentResolve } from './resolvers/department-resolve'
+import { InitResolver } from './resolvers/init-resolve.service'
+import { DepartmentResolver } from './routes/my-content/resolvers/department-resolv.servive'
+import { AllContentComponent } from './routes/my-content/components/all-content/all-content.component'
+// import { PageResolve } from '@sunbird-cb/utils'
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'directory',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    resolve: {
+      department: DepartmentResolve,
+    },
+    children: [
+      {
+        path: 'users',
+        component: UsersViewComponent,
+        children: [],
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'roles-access',
+        component: RolesAccessComponent,
+      },
+      {
+        path: 'directory',
+        component: DirectoryViewComponent,
+      },
+      // {
+      //   path: ':department/create-department',
+      //   component: CreateMdoComponent,
+      // },
+      // {
+      //   path: 'create-user',
+      //   component: CreateUserComponent,
+      // },
+      {
+        path: 'cbp',
+        component: AllContentComponent,
+        data: { load: ['ordinals', 'ckeditor', 'meta'] },
+        resolve: {
+          script: InitResolver,
+          departmentData: DepartmentResolver, // comment for sunbird BE to work should be removed
+        },
+      }
+    ],
+  },
+
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  providers: [
+    HomeResolve,
+    DepartmentResolve,
+    InitResolver,
+    DepartmentResolver,
+  ],
+})
+export class HomeRoutingModule { }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  sitePath: 'cbc-sb.idc.tarento.com',
+  karmYogiPath: 'https://igot-sunbird.idc.tarento.com',
+}
