bb3d010259ce361e57de71a7fa90fe963a80243e
Keyur Rathod <keyur.rathod84@gmail.com>
2021-05-20 12:04:49 +0530

    Change preprod URL in environment

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  sitePath: 'igot-pp-sunbird-cbc.idc.tarento.com',
-  karmYogiPath: 'https://igot-pp-sunbird.idc.tarento.com',
-  cbpPath: 'https://igot-pp-sunbird-cbp.idc.tarento.com',
+  sitePath: 'cbc.igot-stage.in',
+  karmYogiPath: 'https://igot-stage.in',
+  cbpPath: 'https://cbp.igot-stage.in',
 }
